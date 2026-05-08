78e6d964aaf16922559740967c8655d6dc6eb0cc
Sandipan80 <bhullarsandipan@gmail.com>
2026-05-08 23:30:45 +0530

    update axios instance baseURL to production server

--- utils/axiosInstance.jsx
+++ utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from 'js-cookie'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api", // Ensure this matches your server.js port
+    baseURL: "https://it-backend-6bvo.onrender.com/api", // Ensure this matches your server.js port
     timeout: 10000, 
     withCredentials: true,
 });
